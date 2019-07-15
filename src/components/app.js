a70964d36cfcd7ae9e6dd4dc80687268647fd4d5
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-15 12:02:33 -0600

    figure out at home because the tutorial took too long

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import Home from './pages/about';
+
 
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-      <NavigationContainer />
+      <Router>
+        <div>
+        <NavigationContainer />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route  path="/about-me" component={About} />
+        </Switch>
+        </div>
+        </Router>
         <h1>Sabin Shresthas Portfolio</h1>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
