c3067c8fbe22874e2469dfbc18cc57dab36f2c15
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-15 10:22:34 -0600

    changed states

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,17 +8,25 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio", 
             data:  [ 
-              {title: "Quip"}, 
-              {title: "Eventbrite"}, 
-              {title: "Ministry Safe"},
-              {title: "SwingAway"}
+              { title: "Quip" }, 
+              { title: "Eventbrite" }, 
+              { title: "Ministry Safe" },
+              { title: "SwingAway"  }
             ]
         };
+
+        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
     }
 
     portfolioItems() {
         return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url={"google.com"}/>;
+        })
+    }
+
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: "Something Else"
         })
     }
     
@@ -28,6 +36,10 @@ export default class PortfolioContainer extends Component {
                 <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
+
+                <hr/>
+
+                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
