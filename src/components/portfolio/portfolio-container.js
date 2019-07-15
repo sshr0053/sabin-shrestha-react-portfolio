0c60faa08dda9f475d491ae93b72b8e23d3d54cf
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-15 10:46:43 -0600

    built data filter

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -7,26 +7,28 @@ export default class PortfolioContainer extends Component {
         super();
         this.state = {
             pageTitle: "Welcome to my portfolio", 
-            data:  [ 
-              { title: "Quip" }, 
-              { title: "Eventbrite" }, 
-              { title: "Ministry Safe" },
-              { title: "SwingAway"  }
+            data: [ 
+              { title: "Quip", category: "eCommerce" }, 
+              { title: "Eventbrite", category: "Scheduling"}, 
+              { title: "Ministry Safe", category: "Enterprise" },
+              { title: "SwingAway", category: "eCommerce" }
             ]
         };
 
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+        this.handleFilter = this.handleFilter.bind(this)
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
     }
 
     portfolioItems() {
         return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url={"google.com"}/>;
-        })
-    }
-
-    handlePageTitleUpdate() {
-        this.setState({
-            pageTitle: "Something Else"
         })
     }
     
@@ -35,11 +37,12 @@ export default class PortfolioContainer extends Component {
             <div>
                 <h2>{this.state.pageTitle}</h2>
 
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+
                 {this.portfolioItems()}
 
-                <hr/>
-
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
