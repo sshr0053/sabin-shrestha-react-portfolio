5f4d11116158e2154b32e95cedbaeeb843dc7a53
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-16 10:56:38 -0600

    redered API data

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,17 +9,10 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio",
             isLoading: false, 
-            data: [ 
-              { title: "Quip", category: "eCommerce", slug: 'quip' }, 
-              { title: "Eventbrite", category: "Scheduling", slug: 'eventbrite'}, 
-              { title: "Ministry Safe", category: "Enterprise", slug: 'ministry-safe' },
-              { title: "SwingAway", category: "eCommerce", slug: 'swingaway' }
-            ]
+            data: []
         };
 
         this.handleFilter = this.handleFilter.bind(this);
-        this.getPortfolioItems = this.getPortfolioItems.bind(this);
-
     }
 
     handleFilter(filter) {
@@ -33,7 +26,9 @@ export default class PortfolioContainer extends Component {
     getPortfolioItems() {
         axios.get("https://sabinshrestha.devcamp.space/portfolio/portfolio_items")
       .then(response =>  {
-        console.log("response data", response);
+        this.setState({
+            data: response.data.portfolio_items
+        })
       })
       .catch(error => {
         console.log(error);
@@ -42,17 +37,19 @@ export default class PortfolioContainer extends Component {
 
     portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug}/>;
-        })
+            return ( <PortfolioItem title={item.name} url={item.url} slug={item.id}/>
+            );
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
     }
     
     render() {
         if(this.state.isLoading) {
             return <div>Loading...</div>;
         }
-
-        this.getPortfolioItems();
-
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
