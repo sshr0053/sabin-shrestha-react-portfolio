903a0b2dd71ee9833f3764453f0bfbc7eb0b3a43
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-17 10:07:26 -0600

    started on nav bar

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,14 +8,19 @@ export default class NavigationComponent extends Component {
 
     render() {
         return (
-            <div>
+            <div className="nav-wrapper">
+                <div className="left-side">
                 <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-
                 <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
                 <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
                 <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
 
                 {false ? <button>Add Blog</button> : null}
+                </div>
+
+                <div className="right-side">
+                SABIN SHRESTHA
+                </div>
             </div>
         )
     }
