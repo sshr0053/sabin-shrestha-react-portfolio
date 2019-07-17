4aa926167a77cec68aa3cb3f02662c0e3b863aef
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-17 11:33:54 -0600

    got rid of momment

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from './navigation/navigation-container';
@@ -18,8 +17,7 @@ export default class App extends Component {
       <div className='app'>
       <Router>
         <div>
-        <h1>Sabin Shresthas Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+
         <NavigationContainer />
 
         <Switch>
