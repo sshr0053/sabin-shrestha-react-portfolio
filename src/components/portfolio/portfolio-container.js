781d1b3c4b82c6b7371e30ce033b65b4637bf9e8
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-17 08:55:40 -0600

    added debugger

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -37,7 +37,7 @@ export default class PortfolioContainer extends Component {
 
     portfolioItems() {
         return this.state.data.map(item => {
-            console.log("portfolio item", item);
+            debugger;
             return ( 
             <PortfolioItem 
             key={item.id}
