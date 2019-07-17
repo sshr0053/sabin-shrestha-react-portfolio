ea70a367009e2bbdaa23869b07ac7bd566d6f697
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-17 11:53:48 -0600

    built style structure for homepage

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -63,7 +63,7 @@ export default class PortfolioContainer extends Component {
                 <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
                 <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
 
-                {this.portfolioItems()}
+                <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
 
             </div>
         )
