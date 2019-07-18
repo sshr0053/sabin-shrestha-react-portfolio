f357a5213102b9a3d53b65b71156e015fe7dd62d
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-18 10:00:11 -0600

    added Event Listener

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-export default function(props) {
-    const { id, description, thumb_image_url, logo_url} = props.item;
+export default class PortfolioItem extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            PortfolioItemClass: ""
+        };
+    }
+
+    handleMouseEnter() {
+        this.setState({PortfolioItemClass: "image-blur" });
+    }
+
+    handleMouseLeave() {
+        this.setState({PortfolioItemClass: ""});
+    }
+
+    render() {
+    const { id, description, thumb_image_url, logo_url} = this.props.item;
     return (
-        <div className="portfolio-item-wrapper">
+        <div className="portfolio-item-wrapper"
+        onMouseEnter ={() => this.handleMouseEnter()}
+        onMouseLeave ={() => this.handleMouseLeave()}
+        
+        >
             <div
-                className="portfolio-img-background"
+                className={" portfolio-img-background " + this.state.PortfolioItemClass}
                 style={{
                     backgroundImage: "url(" + thumb_image_url + ")"
                 }}
@@ -20,5 +41,6 @@ export default function(props) {
             <div className="subtitle">{description}</div>
             </div>
         </div>
-    )
+    );
+  }
 }
