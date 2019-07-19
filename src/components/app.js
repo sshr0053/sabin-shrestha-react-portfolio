4deba08d480ca3015642db49f031b316c3e6f717
codestudent <codestudent@708S-G104-112.canyonsdistrict.pvt>
2019-07-19 09:41:36 -0600

    Debugging time

--- src/components/app.js
+++ src/components/app.js
@@ -10,8 +10,6 @@ import PortfolioDetail from './portfolio/portfolio-detail';
 import Auth from './pages/auth';
 import NoMatch from './pages/no-match';
 
-
-
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -22,27 +20,25 @@ export default class App extends Component {
 
     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
     this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
-
   }
 
   handleSuccessfulLogin() {
-    this.setState = ({
+    this.setState({
       loggedInStatus: "LOGGED_IN"
-    })
+    });
   }
 
   handleUnSuccessfulLogin() {
-    this.state = ({
+    this.setState({
       loggedInStatus: "NOT_LOGGED_IN"
-    })
+    });
   }
 
   render() {
     return (
-      <div className='container'>
+      <div className="container">
       <Router>
         <div>
-
         <NavigationContainer />
 
         <Switch>
@@ -56,8 +52,7 @@ export default class App extends Component {
                 handleSuccessfulLogin = {this.handleSuccessfulLogin}
                 handleUnsuccessfulLogin = {this.handleUnsuccessfulLogin}
                 />
-            )
-            }
+            )}
             />
 
           <Route path="/about-me" component={About} />
@@ -74,3 +69,85 @@ export default class App extends Component {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import NavigationContainer from "./navigation/navigation-container";
+// import Home from "./pages/home";
+// import About from "./pages/about";
+// import Contact from "./pages/contact";
+// import Blog from "./pages/blog";
+// import PortfolioDetail from "./portfolio/portfolio-detail";
+// import Auth from "./pages/auth";
+// import NoMatch from "./pages/no-match";
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       loggedInStatus: "NOT_LOGGED_IN"
+//     };
+
+//     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+//     this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+//   }
+
+//   handleSuccessfulLogin() {
+//     this.setState({
+//       loggedInStatus: "LOGGED_IN"
+//     });
+//   }
+
+//   handleUnSuccessfulLogin() {
+//     this.setState({
+//       loggedInStatus: "NOT_LOGGED_IN"
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <Router>
+//           <div>
+//             <NavigationContainer />
+
+//             <Switch>
+//               <Route exact path="/" component={Home} />
+
+//               <Route
+//                 path="/auth"
+//                 render={props => (
+//                   <Auth
+//                     {...props}
+//                     handleSuccessfulLogin={this.handleSuccessfulLogin}
+//                     handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+//                   />
+//                 )}
+//               />
+
+//               <Route path="/about-me" component={About} />
+//               <Route path="/contact" component={Contact} />
+//               <Route path="/blog" component={Blog} />
+//               <Route
+//                 exact
+//                 path="/portfolio/:slug"
+//                 component={PortfolioDetail}
+//               />
+//               <Route component={NoMatch} />
+//             </Switch>
+//           </div>
+//         </Router>
+//       </div>
+//     );
+//   }
+// }
